05858490fde02b42fda60a34b7e9a3675b370378
Ken Brown <kbrown@cornell.edu>
2011-04-20 11:25:55 -0400

    Add copyright and licensing information.
    * COPYING: New file.
    * coset.cc: Add copyright and licensing information.
    * coset.h: Ditto.
    * cosettable.cc: Ditto.
    * cosettable.h: Ditto.
    * gens_and_words.cc: Ditto.
    * gens_and_words.h: Ditto.
    * main.cc: Ditto.

--- coset.cc
+++ coset.cc
@@ -1,3 +1,24 @@
+/* coset.cc: the Coset class.
+
+   Copyright 2011 Kenneth S. Brown.
+
+   This file is part of toddcox.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version (GPLv3+).
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://gnu.org/licenses/>.
+
+   Written by Ken Brown <kbrown@cornell.edu>. */
+
 #include "coset.h"
 #include "gens_and_words.h"
 #include <iostream>
--- gens_and_words.cc
+++ gens_and_words.cc
@@ -1,3 +1,24 @@
+/* gens_and_words.cc: manipulating generators and words.
+
+   Copyright 2011 Kenneth S. Brown.
+
+   This file is part of toddcox.
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 3 of the
+   License, or (at your option) any later version (GPLv3+).
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, see <http://gnu.org/licenses/>.
+
+   Written by Ken Brown <kbrown@cornell.edu>. */
+
 #include "gens_and_words.h"
 
 int
