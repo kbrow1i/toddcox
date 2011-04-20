8b250053d79fc821424fc749e5b1d908e3683c87
Ken Brown <kbrown@cornell.edu>
2011-04-20 12:30:43 -0400

    Minor changes to CosetTable; add TODO file.
    * cosettable.h (CosetTable): Rearrange members.
    * TODO: New file.

--- cosettable.h
+++ cosettable.h
@@ -31,29 +31,29 @@ class CosetTable
 {
 public:
   CosetTable (vector<word> rel, vector<word> gen_H);
-  void define (int k, int x);
-  bool is_defined (int k, int x) const { return (tab[k].get_act (x) >= 0); };
-  void print (bool standard = true) const;
-  void debug_print () const;
-  void scan_and_fill (int k, const word& w);
-  void scan (int k, const word& w);
-  bool is_alive (int k) const { return (p[k] == k); };
-  int get_size () const { return tab.size (); };
-  void hlt ();
-  bool hlt_plus (int threshold);
-  void lookahead ();
-  int get_nlive () const;
+  void hlt ();			/* HLT algorithm... */
+  bool hlt_plus (int threshold); /* ...with lookahead */
   void compress ();
   void standardize ();
+  int get_nlive () const;
+  int get_size () const { return tab.size (); };
+  void print (bool standard = true) const;
+  void debug_print () const;
 private:
   vector<Coset> tab;
   vector<int> p;		// for generating equivalence classes; p[k] <= k
   queue<int> q;			// dead cosets to be processed
+  vector<word> relator;
+  vector<word> generator_of_H;
+  void lookahead ();
+  void scan_and_fill (int k, const word& w);
+  void scan (int k, const word& w);
+  bool is_alive (int k) const { return (p[k] == k); };
+  void define (int k, int x);
+  bool is_defined (int k, int x) const { return (tab[k].get_act (x) >= 0); };
   int rep (int c);
   void merge (int k, int l);
   void coincidence(int k, int l);
-  vector<word> relator;
-  vector<word> generator_of_H;
   void swap (int k, int l);
 };
 
